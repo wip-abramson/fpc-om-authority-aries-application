165b570872e06750a6e7f674eb48453e4d35b138
wip-abramson <wip.abramson@gmail.com>
2021-05-22 18:15:28 +0100

    update production server url

--- client/src/api/instance.js
+++ client/src/api/instance.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const base_endpoint = process.env.NODE_ENV === 'production' ? 'http://0.0.0.0:8000' :  'http://0.0.0.0:8000'
+const base_endpoint = process.env.NODE_ENV === 'production' ? 'http://139.162.224.50:8000' :  'http://0.0.0.0:8000'
 // const base_endpoint = 'http://194.61.20.127:8000'
 export const apiInstance = axios.create({
     baseURL: base_endpoint
